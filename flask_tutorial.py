eed2674932c5b18734c7b108cbc637f9d1eea89f
sourshree <120038487+sourshree@users.noreply.github.com>
2023-06-09 16:24:31 +0530

    Update flask_tutorial.py

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -2,14 +2,6 @@ from flask import Flask,render_template,request
 #to make our own web server
 app = Flask('server1')
 visited=0
-@app.route('/hi')
-def index():
-    return render_template('search.html')
-
-@app.route('/shree')
-def aa():
-    print('i visited')
-    return 
 @app.route('/counter')
 def set():
     global visited
